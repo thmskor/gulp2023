f2a0ebfaadf8e1a7b488ff1b3389322efee124c0
thmskor <korchagin.art@gmail.com>
2023-03-09 01:13:07 +0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const { src, dest, watch, parallel } = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+
+
+function scripts() {
+  return src('src/js/app.js')
+  .pipe(concat('app.main.js'))
+  .pipe(uglify())
+  .pipe(dest('src/js'))
+  .pipe(browserSync.stream())
+}
+
+function styles() {
+  return src('src/scss/app.scss')
+  .pipe(concat('app.min.css'))
+  .pipe(scss({ outputStyle: 'compressed' }))
+  .pipe(dest('src/css'))
+  .pipe(browserSync.stream())
+}
+
+function watching() {
+  watch(['src/scss/app.scss'], styles)
+  watch(['src/js/app.js'], scripts)
+  watch(['src/*.html']).on('change', browserSync.reload)
+}
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "src/"
+    }
+  });
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(styles, scripts, browsersync, watching);
